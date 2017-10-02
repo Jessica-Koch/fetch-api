eca554e01efc4e7086eca2b6a90461567981fe11
JessicaKoch <jessicakoch136@icloud.com>
2017-10-01 19:16:29 -0700

    able to create models for users

--- server/config/config.js
+++ server/config/config.js
@@ -1,34 +1,26 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DATABASE_NAME,
     host: process.env.RDS_DB_ENDPOINT,
-    port: 5434,
-    logging: console.log,
-    maxConcurrentQueries: 100,
-    dialect: "postgres",
-    dialectOptions: {
-        ssl: "Amazon RDS"
-    },
-    pool: {
-        maxConnections: 5,
-        maxIdleTime: 30
-    },
-    language: 'en'
+    port: 5432,
+    dialect: "postgres"
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
   }
 }
