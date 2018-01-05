98b566840e8425955698ff82dc730da42572f10a
Jessica-Koch <jessicakoch136@icloud.com>
2018-01-04 19:59:21 -0800

    can now grab user that has just been authenticated with JWT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const AuthController = require('./server/controllers/AuthController');
-
+const path = require('path');
 const app = express();
 // app.use(
 //   session({
@@ -16,14 +16,18 @@ const app = express();
 // );
 // app.use(passport.initialize());
 // app.use(passport.session());
-app.use(logger('dev'));
+app.use(
+  logger(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
+  )
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Require our routes into the application
 require('./server/routes')(app);
-app.get('*', (req, res) => res.status(200).send('root'));
 app.use('/api/auth', AuthController);
+app.get('*', (req, res) => res.status(200).send(req));
 
 module.exports = app;
